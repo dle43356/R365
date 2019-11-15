be1de1fb985900c66e814c3e263550029a67ae1b
Dat <c54bravo@yahoo.com>
2019-11-15 07:43:19 -0600

    Stretch goal 2 complete
    Allow application to process entered entries until Ctrl+c is used

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -6,23 +6,34 @@ namespace StringCalculator
 {
     class Program
     {
+        private static bool keepRunning = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter numbers to add");
-            var input = Console.ReadLine();
-            // In production, would use DI
-            try
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ExitHandler);
+            while (keepRunning)
             {
-                var calculatorService =
-                    new CalculatorService(new InputProcessorService(new DelimiterHelper()),
-                                          new ValidatorService());
-                Console.WriteLine(calculatorService.Calculate(input));
+                Console.WriteLine("Please enter numbers to add");
+                var input = Console.ReadLine();
+                // In production, would use DI
+                try
+                {
+                    var calculatorService =
+                        new CalculatorService(new InputProcessorService(new DelimiterHelper()),
+                                              new ValidatorService());
+                    Console.WriteLine($"Your result is: {calculatorService.Calculate(input)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine();
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.Read();
+        }
+
+        private static void ExitHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            args.Cancel = true;
+            keepRunning = false;
         }
     }
 }
